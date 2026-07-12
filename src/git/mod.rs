155e8a868677442f688031d4664f45b44d9412c2
DeltaX259 <88389578+DeltaX259@users.noreply.github.com>
2026-07-12 16:39:19 +0100

    functional diff selecter and viewer

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -5,3 +5,4 @@ pub mod branch;
 pub mod clone;
 pub mod commit;
 pub mod diff;
+pub mod diff2;
--- src/main.rs
+++ src/main.rs
@@ -213,7 +213,7 @@ fn main() -> ExitCode {
             }
         }
         Commands::Diff { file } => {
-            if let Err(e) = git::diff::diff(file) {
+            if let Err(e) = git::diff2::diff2(file) {
                 eprintln!("[Error]: {e}");
                 return ExitCode::FAILURE;
             }
